e1fd9d7cf8686ea977777c491edbe32a6af4a435
Wander Hayashida <wrhbra2013@gmail.com>
2026-07-03 23:02:23 -0300

    v1.0.6

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Dica de URL para autodescoberta da API
 // O token é obtido automaticamente via GET /api-key
 window.API_CONFIG = {
-    baseUrl: ''
+    baseUrl: 'https://api.projetosdinamicos.com.br/dinheiro/api/'
 };
